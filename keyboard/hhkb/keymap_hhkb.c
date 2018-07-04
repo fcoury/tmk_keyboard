45789952ec375ba026c38ad40f6466bf2b9ebd23
Felipe Coury <felipe.coury@gmail.com>
2018-07-04 00:58:23 -0400

    feat: added HHKB GESC

--- keyboard/hhkb/keymap_hhkb.c
+++ keyboard/hhkb/keymap_hhkb.c
@@ -22,7 +22,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      *       |Alt|Gui  |         Space         |Gui  |Alt|
      *       `-------------------------------------------'
      */
-    KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV,   \
+    KEYMAP(FN1, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV,   \
            TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,       \
            LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,             \
            LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,FN0,             \
@@ -41,9 +41,9 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      *       |   |     |                       |     |   |
      *       `-------------------------------------------'
      */
-    KEYMAP(PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL,   \
+    KEYMAP(FN1, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL,   \
            CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS, UP, TRNS, BSPC,      \
-           TRNS,VOLD,VOLU,MUTE,TRNS,TRNS,PAST,PSLS,HOME,PGUP,LEFT,RGHT,PENT,            \
+           TRNS,MUTE,VOLD,VOLU,TRNS,TRNS,PAST,PSLS,HOME,PGUP,LEFT,RGHT,PENT,            \
            TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,TRNS,TRNS,            \
                 TRNS,TRNS,          TRNS,               TRNS,TRNS),
 };
@@ -89,7 +89,28 @@ const action_t fn_actions[] __attribute__ ((section (".keymap.fn_actions"))) = {
     [31] = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_BSLASH),
 };
 #else
+enum function_id {
+	ESCAPE
+};
+
 const action_t fn_actions[] PROGMEM = {
     [0]  = ACTION_LAYER_MOMENTARY(1),
+    [1]  = ACTION_FUNCTION(ESCAPE)
 };
+
+void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+	if(id == ESCAPE){
+		void (*method)(uint8_t) = (record->event.pressed) ? &add_key : &del_key;
+		uint8_t shifted = get_mods() & (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT));
+        uint8_t alted = get_mods() & (MOD_BIT(KC_LGUI) | MOD_BIT(KC_RGUI));
+
+		if(layer_state == 0)
+			method(shifted || alted ? KC_GRAVE : KC_ESCAPE);
+		else
+			method(shifted || alted ? KC_ESCAPE : KC_GRAVE);
+
+		send_keyboard_report();
+	}
+}
 #endif
